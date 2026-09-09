c63b06ad4dd100d5b07239204b0ad602d50b0e0c
xshare.cc <nicbest@163.com>
2026-09-10 01:16:45 +0800

    fix: install hdblog refer resolver

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './userscript.js';
 import { installHdblogArticleEnhancement } from './hdblog-article.js';
 import { installHdblogImageHostSettings } from './hdblog-image-hosts.js';
 import { installHdblogPreviewImages } from './hdblog-preview.js';
+import { installHdblogReferResolver } from './hdblog-refer.js';
 import { installHdblogSearchEnhancement } from './hdblog-search.js';
 
 installHdblogImageHostSettings();
+installHdblogReferResolver();
 installHdblogArticleEnhancement();
 installHdblogPreviewImages();
 installHdblogSearchEnhancement();
