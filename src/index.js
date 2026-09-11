a7130087de82acd138ec2799c37a695a84b7f794
xshare.cc <nicbest@163.com>
2026-09-11 09:09:32 +0800

    refactor: keep hdblog and x1080x settings separate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './userscript.js';
 import {
-  finishUnifiedSettingsMenuInstall,
   installAgaghhhEnhancement,
-  installUnifiedSettingsMenu,
+  installX1080xSettingsMenu,
 } from './agaghhh-enhancement.js';
 import { installHdblogArticleEnhancement } from './hdblog-article.js';
 import { installHdblogImageHostSettings } from './hdblog-image-hosts.js';
@@ -10,11 +9,10 @@ import { installHdblogPreviewImages } from './hdblog-preview.js';
 import { installHdblogReferResolver } from './hdblog-refer.js';
 import { installHdblogSearchEnhancement } from './hdblog-search.js';
 
-installUnifiedSettingsMenu();
+installX1080xSettingsMenu();
 installAgaghhhEnhancement();
 installHdblogImageHostSettings();
 installHdblogReferResolver();
 installHdblogArticleEnhancement();
-finishUnifiedSettingsMenuInstall();
 installHdblogPreviewImages();
 installHdblogSearchEnhancement();
