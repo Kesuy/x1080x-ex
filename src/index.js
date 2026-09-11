f89da958a5470bcfbcb588b6b625cf7ca3faf753
xshare.cc <nicbest@163.com>
2026-09-11 09:00:10 +0800

    feat: wire unified settings and agaghhh enhancement

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './userscript.js';
+import {
+  finishUnifiedSettingsMenuInstall,
+  installAgaghhhEnhancement,
+  installUnifiedSettingsMenu,
+} from './agaghhh-enhancement.js';
 import { installHdblogArticleEnhancement } from './hdblog-article.js';
 import { installHdblogImageHostSettings } from './hdblog-image-hosts.js';
 import { installHdblogPreviewImages } from './hdblog-preview.js';
 import { installHdblogReferResolver } from './hdblog-refer.js';
 import { installHdblogSearchEnhancement } from './hdblog-search.js';
 
+installUnifiedSettingsMenu();
+installAgaghhhEnhancement();
 installHdblogImageHostSettings();
 installHdblogReferResolver();
 installHdblogArticleEnhancement();
+finishUnifiedSettingsMenuInstall();
 installHdblogPreviewImages();
 installHdblogSearchEnhancement();
