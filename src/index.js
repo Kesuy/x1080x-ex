66feb7509c12e0156f017e2c0c86c217334ca14c
xshare.cc <nicbest@163.com>
2026-09-13 03:29:56 +0800

    feat: install qBittorrent settings integration

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { installHdblogImageHostSettings } from './hdblog-image-hosts.js';
 import { installHdblogPreviewImages } from './hdblog-preview.js';
 import { installHdblogReferResolver } from './hdblog-refer.js';
 import { installHdblogSearchEnhancement } from './hdblog-search.js';
+import { installQbittorrentSettings } from './qbittorrent-settings.js';
 
 installX1080xSettingsMenu();
 installAgaghhhEnhancement();
+installQbittorrentSettings();
 installHdblogImageHostSettings();
 installHdblogReferResolver();
 installHdblogArticleEnhancement();
