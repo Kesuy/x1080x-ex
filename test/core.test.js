f52c7e018f37e27f30864ca41fd7040bfe840a4a
xshare.cc <nicbest@163.com>
2026-09-19 05:53:55 +0800

    test: keep forum Pixhost validation metadata

--- test/core.test.js
+++ test/core.test.js
@@ -276,6 +276,34 @@ test('FC2-PPV 页面生成指定 torrent 名和 A、B 图片名', () => {
   ]);
 });
 
+test('论坛 Pixhost 图片任务保留 show 地址，供下载前检查失效状态', () => {
+  const dom = new JSDOM(`
+    <span id="thread_subject">EBWH-319 [BT] 示例标题</span>
+    <a href="forum.php?mod=forumdisplay&fid=244">BT</a>
+    <div id="postlist"><div id="post_1"><div id="postmessage_1">
+      <img src="https://agaghhh.cc/cover.jpg" width="1200" height="900">
+      <a href="https://pixhost.to/show/9005/ebwh-319-preview.jpg">
+        <img src="https://t5.pixhost.to/thumbs/9005/ebwh-319-preview.jpg" width="320" height="180">
+      </a>
+    </div></div></div>
+  `, { url: 'https://agaghhh.cc/forum.php?mod=viewthread&tid=1018214&highlight=EBWH-319' });
+
+  const jobs = buildDownloadJobs(dom.window.document).filter((job) => job.kind === 'image');
+  assert.equal(jobs.length, 2);
+  assert.deepEqual(jobs[0], {
+    kind: 'image',
+    url: 'https://agaghhh.cc/cover.jpg',
+    name: 'EBWH-319 A.jpg',
+  });
+  assert.deepEqual(jobs[1], {
+    kind: 'image',
+    url: 'https://t5.pixhost.to/thumbs/9005/ebwh-319-preview.jpg',
+    name: 'EBWH-319 B.jpg',
+    pixhostShowUrl: 'https://pixhost.to/show/9005/ebwh-319-preview.jpg',
+    pixhostThumbUrl: 'https://t5.pixhost.to/thumbs/9005/ebwh-319-preview.jpg',
+  });
+});
+
 test('FC2-PPV 三张及以上图片从第二张开始使用 B1、B2 编号', () => {
   const dom = new JSDOM(`
     <span id="thread_subject">FC2-PPV-4960963 [BT](FC2) 示例标题</span>
